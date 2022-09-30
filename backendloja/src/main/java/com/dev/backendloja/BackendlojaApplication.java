02d0cb61ea2ff90ad8aa484ae03662c84981f984
fg.14.cavalcandido@gmail.com <fg.14.cavalcandido@gmail.com>
2022-09-29 22:19:25 -0300

    first commit

---
+++ backendloja/src/main/java/com/dev/backendloja/BackendlojaApplication.java
@@ -0,0 +1,13 @@
+package com.dev.backendloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendlojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendlojaApplication.class, args);
+	}
+
+}
